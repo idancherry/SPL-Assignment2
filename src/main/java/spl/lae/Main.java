92e0aaa90a2f3deaf3ea06b7b93fa1ecdc9384ab
sarit <sarityar@post.bgu.ac.il>
2025-12-27 03:21:23 +0200

    main

--- src/main/java/spl/lae/Main.java
+++ src/main/java/spl/lae/Main.java
@@ -1,10 +1,50 @@
 package spl.lae;
+import java.io.FileWriter;
 import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
 
 import parser.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
       // TODO: main
+
+      // 1. בדיקת תקינות הקלט הראשוני
+        if (args.length != 3) {
+            System.err.println("Usage: java Main <input_json> <output_json> <num_threads>");
+            return;
+        }
+
+        int numThreads = Integer.parseInt(args[0]);
+        String inputPath = args[1];  
+        String outputPath = args[2];
+
+        try{
+          
+          InputParser parser = new InputParser(); 
+          ComputationNode root = parser.parse(inputPath);
+          
+          LinearAlgebraEngine leaEngine = new LinearAlgebraEngine(numThreads);
+
+          ComputationNode resultNode = leaEngine.run(root);
+          double[][] resultMatrix = resultNode.getMatrix();
+          OutputWriter.write(resultMatrix, outputPath);
+
+
+        } catch (Exception e) {
+            // write error to output file and print to stderr
+            try{
+              OutputWriter.write(e.getMessage(), outputPath);
+              System.err.println("Error: " + e.getMessage());
+              e.printStackTrace();
+
+            } catch (IOException exception){
+              exception.printStackTrace();
+            }
+            e.printStackTrace();
+          }
     }
+    
 }
+
